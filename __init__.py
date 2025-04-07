1451409e6e81468e216ed8e329c89e6446baec65
Ahmad Alismail <afi.89@outlook.com>
2025-04-07 14:35:01 +0200

    update requirements

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from transformers import AutoConfig, AutoModelForSpeechSeq2Seq
-from .dicow_config import DiCoWConfig
-from .modeling_dicow import DiCoWForConditionalGeneration
-
-AutoConfig.register("dicow", DiCoWConfig)
-AutoModelForSpeechSeq2Seq.register(DiCoWConfig, DiCoWForConditionalGeneration)
